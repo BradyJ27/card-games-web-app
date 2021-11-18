cf4675bfd1c39043258261ed51ecbc37745c91f6
BradyJ27 <bradyjohnson84@gmail.com>
2021-11-17 20:07:11 -0600

    Add 2 buttons on home screen, one for each game

---
+++ Controllers/SolitaireController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace card_game_web_app.Controllers
+{
+	public class SolitaireController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
