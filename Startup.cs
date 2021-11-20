a38d779d4c0e77d2695e272804f16f3e2edec682
Daniel Ha <bb13112001@gmail.com>
2021-11-19 18:10:13 -0600

    Update Startup file
    Change start up file so that when run the web, it will show the landing page instead of the home page

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace card_game_web_app
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{controller=Landing}/{action=Index}/{id?}");
 			});
 		}
 	}
