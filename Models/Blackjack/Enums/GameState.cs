f3beab3ab78ff6a9f1c8720137c09c76abd74b06
Jaron David <jaronraj@gmail.com>
2021-12-11 23:37:05 -0600

    Edited GameState.cs to follow our game logic

--- Models/Blackjack/Enums/GameState.cs
+++ Models/Blackjack/Enums/GameState.cs
@@ -9,11 +9,8 @@ namespace Models.Blackjack.Enums
     {
         NotStarted, //Before first hand
         Betting, //During the betting phase
-        Dealing, //Only while dealer is dealing the initial deal
         InProgress, //After the initial deal, but before bets are paid out or collected.
-        Insurance, //During an insurance bet
         Payout, //After the hand is over, during while bets are paid out or collected.
-        Shuffling, //While the dealer is shuffling the cards
-        EscortedOut //Win condition; player won too much money so the casino is kicking them out.
-    }
+        EndOfGame, //Happens when the player runs out of money. 
+    }   
 }
