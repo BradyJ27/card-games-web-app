4f7bbddf8836caee857e5ee36d9ceedca15748a4
Jaron David <jaronraj@gmail.com>
2021-12-15 15:42:13 -0600

    Update GameState.cs

--- Models/Blackjack/Enums/GameState.cs
+++ Models/Blackjack/Enums/GameState.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 
 namespace Models.Blackjack.Enums
 {
+    /* 
+     * This enumeration is a modfied version from a Black Jack game created by ExceptionNotFound.
+     * https://github.com/exceptionnotfound/BlazorGames/blob/master/BlazorGames/Models/Blackjack/Enums/GameState.cs
+     */
     public enum GameState
     {
         NotStarted, //Before first hand
         Betting, //During the betting phase
         InProgress, //After the initial deal, but before bets are paid out or collected.
         Payout, //After the hand is over, during while bets are paid out or collected.
-        EndOfGame, //Happens when the player runs out of money. 
+        EndOfGame, //Happens when the player runs out of money or player exits. 
     }   
 }
